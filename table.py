d729de1ad2e62ab74156a75d83924db2dccc9c62
DanElis <e1iseev98@mail.ru>
2018-09-14 16:22:34 +0700

    add tabs

--- table.py
+++ table.py
@@ -7,14 +7,14 @@ from bokeh.layouts import column, row
 from bokeh.transform import transform,dodge
 from bokeh.events import Tap,Press
 from bokeh.models.widgets import CheckboxGroup
-from bokeh.models.widgets import Panel, Tabs
+from bokeh.models.widgets import Panel, Tabs, RadioButtonGroup
 import os 
 import numpy
 class Page():
 	def __init__(self):
-		fig1 = figure()
-		fig1.circle([0],[0])
-		tab_invsible = Panel(child=fig1, title=' ')
+		self.tabs =  RadioButtonGroup(labels = ['Options1','Options2'],active = 0)
+		self.tabs.on_change('active', lambda attr, old, new: self.change_page())
+
 		self.data_directory = '/home/wer/diplom/flaskapp/flaskapp/data'
 		self.data_files = os.listdir(self.data_directory) 
 
@@ -26,13 +26,22 @@ class Page():
 		self.refresh_button = Button(label="Refresh", button_type="success", width=100)
 		self.refresh_button.on_click(self.update_directory)
 
-		self.layout = column(row(self.select_data_files1,self.select_data_files2),
+		self.layout = column(self.tabs,row(self.select_data_files1,self.select_data_files2),
 						self.refresh_button)
-		self.tab1 = Panel(child = self.layout, title = "data")
-		self.tab2 = Panel(child = self.select_data_files1, title = "plots")
-		self.tabs = Tabs(tabs =[self.tab1,self.tab2,tab_invsible])
 		curdoc().add_root(self.layout)
 		#show(self.tabs)
+
+	def change_page(self):
+		if(self.tabs.active == 0):
+			self.layout.children = [self.tabs,row(self.select_data_files1,self.select_data_files2),
+						self.refresh_button,self.plot_matrix]
+		elif(self.tabs.active == 1):
+			self.layout.children = [self.tabs,
+			self.sel_well,self.pred_button,
+			self.plots_rock,
+			row(column(self.select_df1_corr, self.checkbox_df1),
+				column(self.select_df2_corr,self.checkbox_df2)),
+			self.plot_correlation]
 
 	def update_page(self):
 		if(self.select_data_files1.value == "df1" or self.select_data_files2.value == "df2"):
@@ -42,9 +51,10 @@ class Page():
 		self.df1 = pd.read_csv('data/ml_training_data.csv')
 		self.df2 = pd.read_csv('data/ml_training_data.csv')
 
-		self.columns = [col for col in self.df1.columns if self.is_number(col)]
+		self.columns_df1 = [col for col in self.df1.columns if self.is_number(col)]
+		self.columns_df2 = [col for col in self.df2.columns if self.is_number(col)]
 		
-		self.select_plots = [0 for i in range(len(self.columns)*2)]
+		self.select_plots = [0 for i in range(len(self.columns) + len(self.columns))]
 	
 		wells = self.df1['Well Name'].unique().tolist()
 		wells = [well for well in wells if well not in ['Recruit F9']]
@@ -58,8 +68,8 @@ class Page():
 		self.source_correlation_plot = None
 		self.plot_correlation = self.init_corr()
 		
-		self.select_df1_corr = Select(title="Data frame 1:", value=self.columns[0], options=self.columns)
-		self.select_df2_corr = Select(title="Data frame 2:", value=self.columns[1], options=self.columns)
+		self.select_df1_corr = Select(title="Data frame 1:", value=self.columns_df1[0], options=self.columns_df1)
+		self.select_df2_corr = Select(title="Data frame 2:", value=self.columns_df2[1], options=self.columns_df2)
 		
 		self.select_df1_corr.on_change('value',lambda attr, old, new:self.update_set_data_())
 		self.select_df2_corr.on_change('value',lambda attr, old, new:self.update_set_data_())
@@ -72,16 +82,9 @@ class Page():
 		data_matrix = self.init_data_matrix(self.df1,self.df2)
 		self.plot_matrix = self.draw_matrix(data_matrix)
 		self.plot_matrix.on_event(Tap, self.update_plots)
-		self.layout.children = [row(self.select_data_files1,self.select_data_files2),
-			self.plot_matrix,
-			self.sel_well,self.pred_button,
-			self.plots_rock,
-			row(column(self.select_df1_corr, self.checkbox_df1),
-				column(self.select_df2_corr,self.checkbox_df2)),
-			self.plot_correlation]
-		self.tab1 = Panel(child = self.layout, title = "data")
-		self.tab2 = Panel(child = self.layout, title = "data")
-		#self.tabs.child = [self.tab1,self.tab2]
+		
+		self.layout.children = [self.tabs,row(self.select_data_files1,self.select_data_files2),
+						self.refresh_button,self.plot_matrix]
 
 	def is_number(self,col):
 		if(type(self.df1[col][0]).__name__ == "int64" or type(self.df1[col][0]).__name__ == "float64"):
@@ -93,24 +96,24 @@ class Page():
 		self.data_files = os.listdir(self.data_directory) 
 		self.select_data_files1 = Select(title="Data files 1:",value = "df1", options=["df1"]+self.data_files)
 		self.select_data_files2 = Select(title="Data files 2:",value = "df2", options=["df2"]+self.data_files)
-		self.layout.children[0] = row(self.select_data_files1,self.select_data_files2)
+		self.layout.children[1] = row(self.select_data_files1,self.select_data_files2)
 
 	def init_plots_rock(self):
 		sdata1 = self.df1[self.df1['Well Name'] == self.sel_well.value]
 		yrange1 = self.get_y_range(sdata1)
 		sdata2 = self.df2[self.df2['Well Name'] == self.sel_well.value]
 		yrange2 = self.get_y_range(sdata2)
-		plots = column(row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns)]),
-			row([self.draw_plot_rock(sdata2, col, yrange2, i == 0) for i, col in enumerate(self.columns)]))
+		plots = column(row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns_df1)]),
+			row([self.draw_plot_rock(sdata2, col, yrange2, i == 0) for i, col in enumerate(self.columns_df1)]))
 		return plots
 
 	def init_corr(self):
 		plot_correlation = figure(plot_width = 800, plot_height = 500, title = 'correlation graph')
-		data = pd.DataFrame(data = {'x':self.df1[self.columns[0]],'y':self.df2[self.columns[1]]})
-
+		data = pd.DataFrame(data = {'x':self.df1[self.columns_df1[0]],'y':self.df2[self.columns_df2[1]]})
 		self.source_correlation_plot = ColumnDataSource(data)
 		plot_correlation.scatter(x='x', y='y', source=self.source_correlation_plot, line_color='red', size=2)
 		return plot_correlation
+
 	def toFixed(self,numObj, digits=0):
 	    return f"{numObj:.{digits}f}"
 		
@@ -121,14 +124,14 @@ class Page():
 		return res
 
 	def update_plots(self,event):
-		#self.r.glyph.text_font_size="1pt"
-		length_columns = len(self.columns)
+		length_columns = len(self.columns_df1)  #!!!
 		x = int(event.x)#/self.plot_matrix.width)
 		y = int(event.y) #/self.plot_matrix.height)
-		if(self.select_plots[length_columns+x] == 1 and 
-			self.select_plots[length_columns-y-1] == 1):
+		if(self.select_plots[length_columns+x] == 1 and self.select_plots[length_columns-y-1] == 1):
+
 			self.select_plots[length_columns+x] = 0
 			self.select_plots[length_columns-y-1] = 0
+
 		elif(event.x<0 or event.x > length_columns):
 			if(self.select_plots[length_columns-y-1] == 1):
 				self.select_plots[length_columns-y-1] = 0
@@ -139,8 +142,7 @@ class Page():
 				self.select_plots[length_columns+x] = 0
 			else:
 				self.select_plots[length_columns+x] = 1
-		elif((event.x or event.x > length_columns < 0) and 
-			(event.y < 0) or event.y > length_columns):
+		elif((event.x or event.x > length_columns < 0) and (event.y < 0) or event.y > length_columns):
 			return
 		else:
 			self.select_plots[length_columns+x] = 1
@@ -151,8 +153,8 @@ class Page():
 		sdata2 = self.df2[self.df2['Well Name'] == self.sel_well.value]
 		yrange2 = self.get_y_range(sdata2)
 
-		plots1 = row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns) if self.select_plots[i] == 1])
-		plots2 = row([self.draw_plot_rock(sdata2, col2, yrange2, j == 0) for j, col2 in enumerate(self.columns) if self.select_plots[length_columns+j] == 1])
+		plots1 = row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns_df1) if self.select_plots[i] == 1])
+		plots2 = row([self.draw_plot_rock(sdata2, col2, yrange2, j == 0) for j, col2 in enumerate(self.columns_df2) if self.select_plots[length_columns+j] == 1])
 		
 		self.plots_rock.children = [plots1,plots2]
 		
@@ -186,8 +188,8 @@ class Page():
 		sdata2 = self.df2[self.df2['Well Name'] == self.sel_well.value]
 		yrange2 = self.get_y_range(sdata2)
 
-		plots1 = row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns) if self.select_plots[i] == 1])
-		plots2 = row([self.draw_plot_rock(sdata2, col2, yrange2, j == 0) for j, col2 in enumerate(self.columns) if self.select_plots[len(self.columns)+j] == 1])
+		plots1 = row([self.draw_plot_rock(sdata1, col, yrange1, i == 0) for i, col in enumerate(self.columns_df1) if self.select_plots[i] == 1])
+		plots2 = row([self.draw_plot_rock(sdata2, col2, yrange2, j == 0) for j, col2 in enumerate(self.columns_df2) if self.select_plots[len(self.columns)+j] == 1])
 		
 		self.plots_rock.children =[plots1,plots2]
 		
@@ -199,12 +201,12 @@ class Page():
 		return plot
 
 	def init_data_matrix(self,df1,df2):
-		matrix = {col:[] for col in self.columns}
-		for i,r in enumerate(self.columns):
-			for j,col in enumerate(self.columns):
+		matrix = {col:[] for col in self.columns_df1}  #!!!!
+		for i,r in enumerate(self.columns_df1):
+			for j,col in enumerate(self.columns_df2):
 				matrix[r].append(float(self.toFixed(numpy.corrcoef(df1[r], df2[col])[0,1],3)))
 		data = pd.DataFrame(data = matrix)
-		data.index = list(self.columns)
+		data.index = list(self.columns_df1)
 		data.index.name = 'rows'
 		data.columns.name = 'columns'
 		return data
